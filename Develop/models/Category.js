691f2b41f25edd900ddd79e0a954df5a4318d70d
Cmbailey089 <christopherbailey089@gmail.com>
2023-05-20 14:33:51 -0400

    included columns for products

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,39 +7,15 @@ class Category extends Model {}
 Category.init(
   {
     id: {
-      type:DataTypes.INTEGER,
-      allowNull:false,
-      primarayKey:true,
-      autoIncrement:true
-    },
-    product_name: {
-      type:DataTypes.STRING,
-      allowNull:false
-    },
-    price: {
-      type:DataTypes.DECIMAL(1,2).UNSIGNED.ZEROFILL,
-      allowNull:false,
-      validate: {
-        DECIMAL: true
-      }
-    },
-    stock: {
       type: DataTypes.INTEGER,
-      allowNull:false,
-      defaultValue: 10,
-      validate: {
-        isNumeric:true
-      }
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-
-    category_id: {
-      type: DataTypes.INTEGER,
-       reference: {
-        model:'catorgory',
-        key:'id'
-      }
-  }
- 
+    category_name:{
+      TYPE: DataTypes.STRING,
+      allowNull: TRUE
+    },
   },
   {
     sequelize,
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,40 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primarayKey:true,
+      autoIncrement:true
+    },
+    product_name: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    price: {
+      type:DataTypes.DECIMAL(1,2).UNSIGNED.ZEROFILL,
+      allowNull:false,
+      validate: {
+        DECIMAL: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue: 10,
+      validate: {
+        isNumeric:true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+       reference: {
+        model:'catorgory',
+        key:'id'
+      }
+  }
+ 
   },
   {
     sequelize,
