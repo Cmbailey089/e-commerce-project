8ddc4d5cbc32f8f68f172b1cf640815729af73c0
Cmbailey089 <christopherbailey089@gmail.com>
2023-05-20 17:21:17 -0400

    connected all the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,15 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foriegnKey:'category_id',
+  onDelete: 'CASCADE'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foriegnKey:'catrgory_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through:{
+    model: ProductTag,
+    unique:false
+  }
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through:{
+    model:ProductTag,
+    unique:false
+  }
+})
 module.exports = {
   Product,
   Category,
